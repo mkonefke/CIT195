d09e8d6a84bd38147af32dfe538eb6552e993d59
Makayla Konefke <maky@Makaylas-MacBook-Pro.local>
2025-03-27 01:15:22 -0400

    message

---
+++ Audit/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Audit
+{
+    class Auditor
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+    class Business
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Address { get; set; }
+        public Auditor Auditor { get; set; }
+        
+        public void DisplayBusiness()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Auditor Information");
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Phone: " + Number);
+            Console.WriteLine("Business Information");
+            Console.WriteLine("Name: " + Business.Name);
+            Console.WriteLine("Type: " + Business.Type);
+            Console.WriteLine("Address: " + Business.Address);
+            Console.WriteLine("Auditor: " + Business.Auditor);
+            Console.ResetColor();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Auditor auditor = new Auditor
+            {
+                Name = "Smith",
+                Number = "231-444-2343",
+            };
+      
+            Business business = new Business
+            {
+                Name = "Marvin's",
+                Type = "Grocery Store",
+                Address = "922 8th Street , Traverse City, MI 49686",
+                Auditor = auditor
+            };
+            business.DisplayBusiness();
+        }
+    }
+}
---
+++ Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        var add = (double a, double b) => a + b;
+        var multiply = (double a, double b) => a * b;
+        var lesserValue = (double a, double b) => 
+        {
+                if (a < b)
+                    return a;
+                else
+                    return b;
+        };
+        Console.WriteLine($"Sum of {num1} and {num2}: {add(num1, num2)}");
+        Console.WriteLine($"Product of {num1} and {num2}: {multiply(num1, num2)}");
+        Console.WriteLine($"Smaller number between {num1} and {num2}: {lesserValue(num1, num2)}");
+    }
+}
