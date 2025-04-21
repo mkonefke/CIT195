2d0cc3e3dd1d408aead3c4829d41a12042705105
Makayla Konefke <maky@Makaylas-MacBook-Pro.local>
2025-04-21 16:38:02 -0400

    message

---
+++ operatorOverload/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+public class GameCharacter
+{
+    public string Name { get; set; }
+    public int Level { get; set; }
+
+    public GameCharacter(string name, int level)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public static GameCharacter operator ++(GameCharacter character)
+    {
+        character.Level++;
+        return character;
+    }
+
+    public static bool operator >(GameCharacter c1, GameCharacter c2)
+    {
+        return c1.Level > c2.Level;
+    }
+
+    public static bool operator <(GameCharacter c1, GameCharacter c2)
+    {
+        return c1.Level < c2.Level;
+    }
+
+    public static GameCharacter operator +(GameCharacter c1, GameCharacter c2)
+    {
+        return new GameCharacter($"{c1.Name}-{c2.Name}", c1.Level + c2.Level);
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} (Level {Level})";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        GameCharacter hero = new GameCharacter("SuperMan", 7);
+        GameCharacter villain = new GameCharacter("Villain", 9);
+
+        Console.WriteLine("Original Characters:");
+        Console.WriteLine(hero);
+        Console.WriteLine(villain);
+
+        hero++;
+        Console.WriteLine("After Leveling Up Hero:");
+        Console.WriteLine(hero);
+
+        Console.WriteLine("Who is stronger?");
+        Console.WriteLine(hero > villain ? $"{hero.Name} is stronger" : $"{villain.Name} is stronger");
+
+        GameCharacter fusion = hero + villain;
+        Console.WriteLine("Combining Characters:");
+        Console.WriteLine(fusion);
+    }
+}
--- tempConverter2/obj/Debug/net8.0/tempConverter2.AssemblyInfo.cs
+++ tempConverter2/obj/Debug/net8.0/tempConverter2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tempConverter2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90ce87bcb8a054b23c5d8ade8eeeccef2d3595b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd50da43d106c5a3f78164c835eedc4cdebf041a")]
 [assembly: System.Reflection.AssemblyProductAttribute("tempConverter2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tempConverter2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
